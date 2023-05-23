6aaacb012757e11ca9ced06e4f6a17d0f075c980
weirdscenario <zoubairbenab9779@gmail.com>
2023-05-23 18:27:49 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react' 
+import { useState,useEffect } from 'react'
+import axios from 'axios'
 
-function App() {
+const App = () => {
+ const [users,setData]=useState('')
+  useEffect(()=>{
+    axios.get('https://jsonplaceholder.typicode.com/users').then(res=>{
+      console.log(res.data)
+      setData(res.data)
+    }).catch(err=>{
+      console.log(err)
+    })
+  },[])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    
+      <div>
+        <ul style={{ listStyle: 'none', padding: 0 }}>
+          {users.map((el) => (
+            <li key={el.id} style={{ margin: '5px 0', fontWeight: 'bold' }}>
+              {el.name}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+    
 }
 
-export default App;
+export default App
